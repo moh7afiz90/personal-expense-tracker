6e64506724e52a51e32ff93d77a7adea1f5481c6
moh7afiz90 <moh.7afiz@gmail.com>
2020-02-29 20:45:40 +0300

    added Income Expenses Component

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  makeStyles
+} from '@material-ui/core'
 
-export const IncomeExpenses = () => {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  contentColor: {
+    color: '#000'
+  }
+})
+
+export const IncomeExpenses = props => {
+  const classes = useStyles()
   return (
     <div>
-      <h1>IncomeExpenses</h1>
+      {/* <Card> */}
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Card style={{ backgroundColor: '#c8e6c9' }}>
+            <CardContent>
+              <Typography
+                className={classes.contentColor}
+                variant="h5"
+                component="h5"
+              >
+                Income
+              </Typography>
+              <Typography className={classes.contentColor} component="h5">
+                $ 0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card style={{ backgroundColor: '#ff9e80' }}>
+            <CardContent>
+              <Typography
+                className={classes.contentColor}
+                variant="h5"
+                component="h5"
+              >
+                Expense
+              </Typography>
+              <Typography component="h5" className={classes.contentColor}>
+                $ 0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      {/* </Card> */}
     </div>
   )
 }
